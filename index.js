9b22624590b54a6053c8f299fd16ed1d864a68c6
NiteChill <puissantachille@gmail.com>
2023-11-15 16:36:03 +0100

    desktop version done

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const photos = document.querySelector('.photos'),
   altitudes = document.querySelector('.altitudes'),
   ratings = document.querySelector('.ratings'),
-  conclusions = document.querySelector('.conclusions');
+  conclusions = document.querySelector('.conclusions'),
+  previous = document.querySelector('.previous'),
+  next = document.querySelector('.next'),
+  data = [];
+let pageNbr = 0;
 
-fetch('./data/mountains.json')
-  .then((response) => response.json())
-  .then((json) => buildContainers(json));
+try {
+  fetch('./data/mountains.json')
+    .then((response) => response.json())
+    .then((json) => buildContainers(json));
+} catch (error) {
+  console.log(error);
+} finally {
+  console.log('successful loading');
+}
 
 function buildContainers(res) {
-  const data = [];
   data.push(res);
   data[0].map((el) => createContainers(el));
 }
@@ -51,12 +60,11 @@ function createContainers(item) {
     rating = document.createElement('div');
   for (let i = Math.floor(item.rating); i > 0; i--) {
     stars.push(1);
-    console.log(stars);
     if (i === 1 && item.rating % 1 !== 0) {
       stars.push(0.5);
+      for (let i = 5 - stars.length; i > 0; i--) stars.push(0);
     }
   }
-  for (let i = 5 - stars.length; i > 0; i--) stars.push(0);
 
   rating.innerHTML = `<div class="text-container">
       <span class="material-symbols-outlined">thumbs_up_down</span>
@@ -80,4 +88,53 @@ function createContainers(item) {
     </div>`;
   rating.className = 'rating';
   ratings.append(rating);
+
+  const conclusion = document.createElement('div');
+  conclusion.innerHTML =
+    `<div class="text-container">
+      <span class="material-symbols-outlined">article</span>
+      <p>conclusion</p>
+    </div>
+    <p>${item.conclusion}</p>`;
+  conclusion.className = 'conclusion';
+  conclusions.append(conclusion);
 }
+
+function update() {
+  if (pageNbr === 0) {
+    previous.style.pointerEvents = 'none';
+    previous.style.opacity = '0.5';
+  } else if (pageNbr === data[0].length - 1) {
+    next.style.pointerEvents = 'none';
+    next.style.opacity = '0.5';
+  }
+  const wrappers = [...document.querySelectorAll('.wrapper')];
+  wrappers.map((wrapper) => {
+    wrapper.style.transform = `translateX(${pageNbr * -100}%)`
+  })
+}
+update();
+
+function changePage(direction = 'forward') {
+  if (direction === 'forward') {
+    next.classList.remove('clicked');
+    previous.style.pointerEvents = 'all';
+    previous.style.opacity = '1';
+    pageNbr++;
+  } else {
+    previous.classList.remove('clicked');
+    next.style.pointerEvents = 'all';
+    next.style.opacity = '1';
+    pageNbr--;
+  }
+  update();
+}
+
+previous.addEventListener('mousedown', () => previous.classList.add('clicked'));
+previous.addEventListener('mouseup', () => changePage('backward'));
+previous.addEventListener('touchstart', () => previous.classList.add('clicked'));
+previous.addEventListener('touchend', () => changePage('backward'));
+next.addEventListener('mousedown', () => next.classList.add('clicked'));
+next.addEventListener('mouseup', () => changePage('forward'));
+next.addEventListener('touchstart', () => next.classList.add('clicked'));
+next.addEventListener('touchend', () => changePage('forward'));
